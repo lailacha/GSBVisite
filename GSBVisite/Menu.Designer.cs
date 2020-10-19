1fc6652f739eb50dbc59fae025c996dc2b1581eb
lala charaoui <mats2@LIVE.FR>
2020-10-19 17:59:36 +0200

    Go

---
+++ GSBVisite/Menu.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GSBVisite
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rapportDeVisiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userCo_tbx = new System.Windows.Forms.Label();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rapportDeVisiteToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(274, 42);
+            // 
+            // rapportDeVisiteToolStripMenuItem
+            // 
+            this.rapportDeVisiteToolStripMenuItem.Name = "rapportDeVisiteToolStripMenuItem";
+            this.rapportDeVisiteToolStripMenuItem.Size = new System.Drawing.Size(273, 38);
+            this.rapportDeVisiteToolStripMenuItem.Text = "Rapport de Visite";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Utilisateur connecté: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // userCo_tbx
+            // 
+            this.userCo_tbx.AutoSize = true;
+            this.userCo_tbx.Location = new System.Drawing.Point(574, 24);
+            this.userCo_tbx.Name = "userCo_tbx";
+            this.userCo_tbx.Size = new System.Drawing.Size(0, 25);
+            this.userCo_tbx.TabIndex = 4;
+            this.userCo_tbx.Click += new System.EventHandler(this.userCo_tbx_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userCo_tbx);
+            this.Controls.Add(this.label2);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem rapportDeVisiteToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label userCo_tbx;
+    }
+}
---
+++ GSBVisite/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSBVisite
+{
+    public partial class Menu : Form
+    {
+
+
+        string chaineConnexion;  //ceci permettra la connexion à la base de données //		
+        string userCo;
+        string userStatut;
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public string ChaineConnexion { get => chaineConnexion; set => chaineConnexion = value; }
+        public string UserCo { get => userCo; set => userCo = value; }
+        public string UserStatut { get => userStatut; set => userStatut = value; }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            userCo_tbx.Text = userCo;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userCo_tbx_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
